6d06e2e17469de82ed6e1196bf6304cd86068a8d
Tianyu Yang <53754724+lingrottin@users.noreply.github.com>
2023-08-01 22:40:06 +0800

    Fix url for deploy

--- WslKernelBuild/buildtasks.ts
+++ WslKernelBuild/buildtasks.ts
@@ -1,5 +1,5 @@
 import fs from "fs";
-import path from 'path';
+import path from "path";
 import { buildConfig, startBuildTask } from "./buildHandler/buildHandler";
 export type buildTask = {
   name: string;
@@ -12,32 +12,46 @@ export class buildTasks {
   private tasks: buildTask[] = [];
   private current: number = -1;
   public readFromFile = () => {
-    let obj = JSON.parse(fs.readFileSync(__dirname + path.sep + "tasks.json", "utf-8"));
+    let obj = JSON.parse(
+      fs.readFileSync(__dirname + path.sep + "tasks.json", "utf-8")
+    );
     this.tasks = <buildTask[]>obj.tasks;
     this.current = <number>obj.current;
-    if(this.tasks[this.current].status==='pending' || this.tasks[this.current].status==='building'){
-      this.current-=1;
-      this.next();
-    }
+    try {
+      if (
+        this.tasks[this.current].status === "pending" ||
+        this.tasks[this.current].status === "building"
+      ) {
+        this.current -= 1;
+        this.next();
+      }
+    } catch (e) {}
   };
   public saveToFile = () => {
     let obj = {
       current: this.current,
       tasks: this.tasks,
     };
-    fs.writeFileSync(__dirname + path.sep +"tasks.json", JSON.stringify(obj), "utf-8");
+    fs.writeFileSync(
+      __dirname + path.sep + "tasks.json",
+      JSON.stringify(obj),
+      "utf-8"
+    );
   };
   public next = () => {
     if (this.current + 1 === this.tasks.length) return;
     this.current += 1;
-    this.tasks[this.current].status="building";
+    this.tasks[this.current].status = "building";
     if (!this.buildConfig.check(this.tasks[this.current].config)) {
       this.tasks[this.current].name += " !Config invalid";
       this.tasks[this.current].status = "done";
       this.saveToFile();
       return;
     } else {
-      startBuildTask(this.tasks[this.current].link, this.tasks[this.current].config).then(() => {
+      startBuildTask(
+        this.tasks[this.current].link,
+        this.tasks[this.current].config
+      ).then(() => {
         this.tasks[this.current].status = "done";
         this.saveToFile();
         this.next();
@@ -52,12 +66,15 @@ export class buildTasks {
       config: config,
       status: "pending",
     });
-    if (this.tasks.length <= 1 || this.tasks[this.tasks.length -2].status === "done") 
+    if (
+      this.tasks.length <= 1 ||
+      this.tasks[this.tasks.length - 2].status === "done"
+    )
       this.next();
     this.saveToFile();
     return;
   }
-  public get(){
+  public get() {
     return this.tasks;
   }
   constructor() {
